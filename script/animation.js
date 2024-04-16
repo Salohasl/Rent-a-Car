86e46008df2b2768fd0302a0ee595d2b012cd0da
Maxim Zavidiy <zavidiy1998@mail.ru>
2024-04-16 18:05:10 +0200

    add API

--- script/animation.js
+++ script/animation.js
@@ -63,13 +63,43 @@ function linkUnderline() {
                     if (faq.dataset.num == link.dataset.num) {
                         link.classList.add('active');
                     } else {
-                        link.classList.remove('active');
+                           link.classList.remove('active');
                     }
                 default:
                     link.classList.remove('active');
             }
         }
+
+
     });
 }
 
 linkUnderline();
+
+
+const faqBlocks = document.querySelectorAll('.faq-block');
+const advantages = document.querySelector('.advantages');
+const observerCallback = (entries, observer) => {
+ entries.forEach(entry => {
+    // Check if the faqBlock is intersecting
+    if (entry.isIntersecting) {
+      // Проверяем, пересекается ли faqBlock
+      entry.target.classList.add('anim');
+      // При желании прекратите наблюдение за faqBlock, если вы хотите, чтобы анимация запускалась только один раз.
+      observer.unobserve(entry.target);
+    }
+ });
+};
+// экземпляр IntersectionObserver
+const observer = new IntersectionObserver(observerCallback, {
+ // При необходимости отрегулируйте rootMargin и порог
+ rootMargin: '0px',
+ threshold: .5
+});
+
+//Следите за advantages
+observer.observe(advantages);
+// Следите за каждым faqBlock
+for (let faqBlock of faqBlocks) {
+    observer.observe(faqBlock);
+}
